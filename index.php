63bf08d695f830bb08a50007af044526422e596c
Ikuo Degawa <degawa@ikuwow.com>
2015-05-15 21:47:33 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="ja">
+<head>
+    <meta charset="utf-8"/>
+</head>
+<body>
+<h1>Guest Book</h1>
+
+<p>Hello World.</p>
+
+<script type="text/javascript" src="./common.js"></script>
+</body>
+</html>
