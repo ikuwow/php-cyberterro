32968ffc9d3ba023381cfdc61c3473802df6c6bb
Ikuo Degawa <degawa@ikuwow.com>
2015-05-16 00:30:06 +0900

    Create basic files

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
+<?php require_once 'form.php'; ?>
 <html lang="ja">
 <head>
     <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="./common.css"/>
 </head>
 <body>
+
+<div class="container">
+
 <h1>Guest Book</h1>
 
 <p>Hello World.</p>
 
+</div><!-- .container -->
 <script type="text/javascript" src="./common.js"></script>
 </body>
 </html>
+
