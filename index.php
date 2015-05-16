34af38e513d389f17098c7e94ac4fdeba8ed21b7
Ikuo Degawa <degawa@ikuwow.com>
2015-05-16 13:38:22 +0900

    Create post list and delete form

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 <p>Hello World.</p>
 
 <div class="form-box">
-
+<h2>Post Form:</h2>
 <form taget="form.php" method="post">
     <label>Title: <input type="text" name="post[title]"/></label>
     <label>name: <input type="text" name="post[name]"/></label>
@@ -35,8 +35,30 @@
     <label>Password: <input type="text" name="post[password]"/></label>
     <input type="submit" value="Submit" />
 </form>
-
 </div><!-- .form-box -->
+
+
+<div class="post-list-box">
+<h2>Post List</h2>
+<?php if(isset($posts)): ?>
+    <?php foreach($posts as $post): ?>
+        <div class="single-post">
+            <p></p>aaa
+        </div>
+    <?php endforeach; ?>
+<?php else: ?>
+    <p>投稿なし</p>
+<?php endif;?>
+</div><!-- .post-list-box -->
+
+
+<h2>Delete Form</h2>
+
+<form taget="form.php" method="post">
+    <label>Post ID: <input type="text" name="delete[id]"/></label>
+    <label>Password: <input type="text" name="delete[password]"/></label>
+    <input type="submit" value="Submit" />
+</form>
 
 </div><!-- .container -->
 <script type="text/javascript" src="./common.js"></script>
